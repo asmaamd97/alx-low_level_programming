8cfd5dff6030a015634dac7b4c369e28f8654575
asmaamsaad97 <asmaamsaad97@gmail.com>
2023-09-14 15:32:05 -0700

    new file

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -23,8 +23,8 @@ int mul(int a, int b)
  */
 int main(void)
 {
-    printf("%d\n", mul(98, 1024));
-    printf("%d\n", mul(-402, 4096));
-    return (0);
+ printf("%d\n", mul(98, 1024));
+ printf("%d\n", mul(-402, 4096));
+ return (0);
 }
 }
